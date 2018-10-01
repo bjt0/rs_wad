463df93865d78900ed5a3e98ef24c41f90c180df
bjt0 <>
2018-10-02 07:44:14 +0930

    add skeleton wad structure with file loading functions

---
+++ src/wad.rs
@@ -0,0 +1,37 @@
+pub enum WadType {
+    IWAD, PWAD
+}
+
+pub struct Header {
+
+}
+
+pub struct Directory {
+
+}
+
+pub struct Wad {
+    header: Header,
+    directory: Directory
+}
+
+impl Wad {
+    pub fn from_path(path: &str) -> Wad {
+        let path = Path::new(path);
+
+        if !path.exists() {
+            panic!("WAD file {} not found!", path.display());
+        }
+
+        let wad_file = match File::open(path) {
+            Ok(file) => file, 
+            Err(why) => panic!("Unable to open WAD {}", why.description());
+        };
+
+        Wad::from_file(&wad_file)
+    }
+
+    pub fn from_file(mut file: &File) -> Wad {
+
+    }
+}
