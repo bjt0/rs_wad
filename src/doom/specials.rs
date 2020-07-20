9d2861d123da2ed9d012c876f75d43741d751883
BJT <23147104+bjt0@users.noreply.github.com>
2020-07-21 04:42:40 +0930

    Complete DOOM II 1.9 door specials list

--- src/doom/specials.rs
+++ src/doom/specials.rs
@@ -33,5 +33,37 @@ pub enum DoorLineSpecial {
     FastOpenStaySingle = 118,
     SlowOpenStaySwitch = 61,
     FastOpenStaySwitch = 115,
-    SlowOpenStaySwitchSingle = 103
+    SlowOpenStaySwitchSingle = 103,
+    FastOpenStaySwitchSingle = 112,
+    SlowOpenStayWalkover = 86,
+    FastOpenStayWalkover = 106,
+    SlowOpenStayWalkoverSingle = 2,
+    FastOpenStayWalkoverSingle = 109,
+    SlowOpenStayShootable = 46,
+    SlowCloseStaySwitch = 42,
+    FastCloseStaySwitch = 116,
+    SlowCloseStaySwitchSingle = 50,
+    FastCloseStaySwitchSingle = 113,
+    SlowCloseStayWalkover = 75,
+    FastCloseStayWalkover = 107,
+    SlowCloseStayWalkoverSingle = 3,
+    FastCloseStayWalkoverSingle = 110,
+    SlowCloseWaitOpenWalkover = 76,
+    SlowCloseWaitOpenWalkoverSingle = 16,
+    // locked door types
+    SlowOpenStayBlueKeySingle = 32,
+    SlowOpenStayRedKeySingle  = 33,
+    SlowOpenStayYellowKeySingle = 34,
+    // fast lock
+    FastOpenStayBlueKeySwitch = 99,
+    FastOpenStayRedKeySwitch  = 134,
+    FastOpenStayYellowKeySwitch = 136,
+    // fast lock switch
+    FastOpenStayBlueKeySwitchSingle = 133,
+    FastOpenStayRedKeySwitchSingle  = 135,
+    FastOpenStayYellowKeySwitchSingle = 137,
+    // fast press repeatable
+    SlowOpenWaitCloseBlueKey = 26,
+    SlowOpenWaitCloseRedKey  = 28,
+    SlowOpenWaitCloseYellowKey = 27,
 }
